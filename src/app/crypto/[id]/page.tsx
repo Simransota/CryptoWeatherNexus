317fb9aca93f22782a6cffd80d242ecc5b923faa
“Simransota” <simran.sota56@gmail.com>
2025-04-01 18:23:01 +0530

    made changes

--- src/app/crypto/[id]/page.tsx
+++ src/app/crypto/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { CryptoDetail } from "@/components/crypto/crypto-detail"
+import { use } from "react"
 
-export default function CryptoDetailPage({ params }: { params: { id: string } }) {
-  return <CryptoDetail cryptoId={params.id} />
+export default function CryptoDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const resolvedParams = use(params)
+  return <CryptoDetail cryptoId={resolvedParams.id} />
 }
